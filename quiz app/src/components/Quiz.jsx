130b95d1abecce65bf1d9628f64106422673908e
Eshwar <eshwarhs170@gmail.com>
2026-05-21 22:38:28 +0530

    completed quiz app

--- quiz app/src/components/Quiz.jsx
+++ quiz app/src/components/Quiz.jsx
@@ -1,29 +1,89 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Quiz.css'
 import { data } from '../assets/data'
 
-const quiz = () => {
+const Quiz = () => {
 
     let [index,setIndex]=useState(0)
-    let [question,setQuestion]=useState(data[index])
+    let [question,setQuestion]=useState(data[index]);
+    let [lock,setLock]=useState(false);
+    let [score,setScore]=useState(0);
+    let [result,setResult]=useState(false)
+
+    let option1=useRef(null);
+    let option2=useRef(null);
+    let option3=useRef(null);
+    let option4=useRef(null);
+
+    let option_array=[option1,option2,option3,option4];
+
+    const checkanswer =(e,ans)=>{
+      if(lock===false){
+      if(question.ans===ans){
+        e.target.classList.add("correct");
+        setLock(true);
+        setScore(prev=>prev+1)
+      }
+      else {
+        e.target.classList.add("wrong");
+        setLock(true);
+        option_array[question.ans-1].current.classList.add("correct")
+      }
+      }
+      
+    }
+
+    const next =()=>{
+      if(lock===true){
+        if(index===data.length-1){
+          setResult(true);
+          return 0;
+        }
+        setIndex(++index);
+        setQuestion(data[index]);
+        setLock(false);
+        option_array.map((option)=>{
+          option.current.classList.remove("wrong")
+          option.current.classList.remove("correct");
+          return null;
+        })
+      }
+    }
+
+    const reset=()=>{
+      setIndex(0);
+      setQuestion(data[0]);
+      setScore(0);
+      setLock(false);
+      setResult(false)
+    }
 
   return (
     <div className='container'>
       <h1>Quizz App</h1>
       <hr />
-      <h2>1.Which device is required for the internet connection?</h2>
+      {result?<></>:<>
+        <h2>{index+1}.{question.question}</h2>
       <ul>
-        <li>Modem</li>
-        <li>Router</li>
-        <li>LAN Cable</li>
-        <li>Pen Drive</li>
+        <li ref={option1} onClick={(e)=>{checkanswer(e,1)}}>{question.option1}</li>
+        <li ref={option2} onClick={(e)=>{checkanswer(e,2)}}>{question.option2}</li>
+        <li ref={option3} onClick={(e)=>{checkanswer(e,3)}}>{question.option3}</li>
+        <li ref={option4} onClick={(e)=>{checkanswer(e,4)}}>{question.option4}</li>
       </ul>
-      <button>Next</button>
+      <button onClick={next}>Next</button>
       <div className="index">
-        1 of 5 questions
+        {index+1} of {data.length} questions
       </div>
+      </>}
+      {result?
+      <>
+       <h2>your Score {score} out of {data.length}</h2>
+      <button onClick={reset}>Reset</button>
+      </>:<></>
+      }
+     
     </div>
   )
 }
 
-export default quiz
+export default Quiz
